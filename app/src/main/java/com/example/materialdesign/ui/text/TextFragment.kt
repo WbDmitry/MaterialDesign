c0feaab5a696bb595227be340a9f3e6b6414a4e1
wbdmitry <wb.dmitry@yandex.ru>
2022-03-27 14:05:05 +0300

    Поигрался с текстом с помощью Spans

--- app/src/main/java/com/example/materialdesign/ui/text/TextFragment.kt
+++ app/src/main/java/com/example/materialdesign/ui/text/TextFragment.kt
@@ -1,11 +1,16 @@
 package com.example.materialdesign.ui.text
 
+import android.graphics.BlurMaskFilter
 import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.materialdesign.R
 import com.example.materialdesign.databinding.FragmentTextBinding
@@ -34,6 +39,69 @@ class TextFragment : Fragment() {
             binding.font2Text1TextView.typeface =
                 resources.getFont(R.font.mightsilly_bwny_b)
         }
+
+        val text = "The foundational elements of print "
+        val text2 =
+            "based design typography, grids, space, scale, color, and use of imagery guide visual treatments."
+
+        val spannableStringBuilder = SpannableStringBuilder(text)
+        spannableStringBuilder.setSpan(
+            StyleSpan(Typeface.BOLD_ITALIC),
+            0,
+            16,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val fontSizeInPx = 70
+        spannableStringBuilder.setSpan(
+            AbsoluteSizeSpan(fontSizeInPx),
+            17,
+            34,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableStringBuilder.insert(text.length, text2)
+
+        val colorText = ContextCompat.getColor(requireContext(), R.color.red_900)
+        spannableStringBuilder.setSpan(
+            ForegroundColorSpan(colorText),
+            text.length,
+            text.length + 12,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val colorBg = ContextCompat.getColor(requireContext(), android.R.color.holo_blue_light)
+        spannableStringBuilder.setSpan(
+            BackgroundColorSpan(colorBg),
+            text.length + 13,
+            text.length + 45,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val blurRadius = 5f
+        val blurMaskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.SOLID)
+        spannableStringBuilder.setSpan(
+            MaskFilterSpan(blurMaskFilter),
+            text.length + 45,
+            text.length + 60,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableStringBuilder.setSpan(
+            StrikethroughSpan(),
+            text.length + 60,
+            text.length + 83,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableStringBuilder.setSpan(
+            UnderlineSpan(),
+            text.length + 84,
+            spannableStringBuilder.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        binding.spansTextView.text = spannableStringBuilder
     }
 
     companion object {
